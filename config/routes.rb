c7991517b58504f1a21e2042638fb2935e16e955
Alexander Kustov <knuxus@gmail.com>
2015-11-21 11:38:37 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  get 'api/stations'
-  get 'api/possibleTravellingTrains'
+  get 'api/cashtrain/stations'
+  get 'api/cashtrain/possibleTravellingTrains'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
