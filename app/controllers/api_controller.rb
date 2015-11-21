7880baf406865085f36e76a1faeb2a3b0e8b206e
Alexander Kustov <knuxus@gmail.com>
2015-11-21 16:50:13 +0000

    list of train data

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,5 @@
 class ApiController < ApplicationController
   include HTTParty
-  base_uri "http://darwin.hacktrain.com/api/train"
   protect_from_forgery only: []
 
   def stations
@@ -10,12 +9,20 @@ class ApiController < ApplicationController
 
   def possibleTravellingTrains
     nearest_station = Station.near([params[:latitude].to_s, params[:longitude].to_s], 5).first
-    @response = self.class.get(URI.encode("http://darwin.hacktrain.com/api/train/#{nearest_station.code}/from?apiKey=b55bbec8-81d3-4ba5-8626-c1c7568a8106&rows=10"))
+    #dev hack
+    if Rails.env.development?
+      params[:destination] = "YRK"
+      nearest_station.code = "KGX"
+    end
+
+    @response = self.class.get(URI.encode("http://darwin.hacktrain.com/api/train/#{params[:destination]}/from/#{nearest_station.code}?apiKey=b55bbec8-81d3-4ba5-8626-c1c7568a8106&rows=10"))
     render json: @response.body
   end
 
   def possibleOffers
     @nearest_station = Station.near([params[:latitude].to_s, params[:longitude].to_s], 5).first
+
+    #dev hack
     if Rails.env.development?
       params[:destination] = "YRK"
       @nearest_station.code = "KGX"
@@ -25,6 +32,7 @@ class ApiController < ApplicationController
     render 'possible_offers', content_type: Mime::JSON
   end
 
+  #POST
   def offers
     Offer.create(permitted_params)
     render json: "success"
